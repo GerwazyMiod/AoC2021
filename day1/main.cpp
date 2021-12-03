f889cb8324bd07d48120836405ef426366907dd7
Exec <pskal@windowslive.com>
2021-12-03 23:23:58 +0100

    update for the second part of solution for day1

--- day1/main.cpp
+++ day1/main.cpp
@@ -4,9 +4,26 @@
 #include <string>
 #include <numeric>
 #include <limits>
+#include <map>
+
+std::tuple<int, int> measurment_window_start_number_and_offset(int measurement_index)
+{
+	int start = (measurement_index > 2) ?  measurement_index - 1 : 1;
+	int offset = (measurement_index > 2) ? 2 : measurement_index;
+	return { start,offset };
+}
+
+
 
 int main()
 {
+	//for (int i = 0; i < 11; i++) {
+	//	auto [start, offset] = measurment_window_start_number_and_offset(i);
+	//	std::cout << "sliding window start:" << start<<std::endl;
+	//	std::cout << "sliding window offset:" << offset <<std::endl;
+	//}
+	//return 0;
+
 	std::ifstream input("input");
 	if (!input) {
 		std::cerr << "Can't open 'input' file!\n";
@@ -32,6 +49,27 @@ int main()
 		previous = current;
 		return sum;
 		});
+
+	std::cout << "Answer is: " << answer;
+
+	std::map<int, int> sliding_windows_sum;
+	const int how_many = std::accumulate(ints_view.begin(), ints_view.end(), 0, [&](int index, int measurment) {
+		auto [start, offset] = measurment_window_start_number_and_offset(index);
+		for (int i = start; i <= (start + offset); ++i) {
+			sliding_windows_sum[i] += measurment;
+		}
+		return ++index;
+	});
+	//erase last two, not complete, windows
+	sliding_windows_sum.erase(sliding_windows_sum.size());
+	sliding_windows_sum.erase(sliding_windows_sum.size());
+
+	previous = std::numeric_limits<int>::max();
+	answer = std::accumulate(sliding_windows_sum.begin(), sliding_windows_sum.end(), 0, [&](int sum, auto window_value) {
+		if (window_value.second > previous) { ++sum; }
+		previous = window_value.second;
+		return sum;
+	});
 
 	std::cout << "Answer is: " << answer;
 
